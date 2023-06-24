072de634099818694b6bf2d50d7840e9e1361f0c
kovardin <artem-kovardin@yandex.ru>
2023-06-25 02:41:38 +0300

    added new tracking methods

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Godot AppMetrica
 
 Плагин AppMetrica для игр на Godot
+
+## Установка
+
+- Cобрать модуль `app-metrica` командой `./gradlew build`
+- Скопировать файлы `app-metrica-release.aar` и `AppMetrica.gdap` в папку `res://android/plugins`
+- Вклчючить плагин в настройках экспорта проекта
+
+![plugins](https://kovardin.ru/img/godot/mytracker/08.png)
+
+## Инициализация
+
+Пример загрузки и использования плагина
+
+```GDScript
+var metrica: Object
+
+func _ready():
+	if Engine.has_singleton("AppMetrica"):
+		metrica = Engine.get_singleton("AppMetrica")
+		
+		metrica.config("xxxxxxxxxx")
+		metrica.withLogs()
+		metrica.init()
+```
--- app-metrica/src/main/java/ru/rustore/appmetrica/AppMetrica.kt
+++ app-metrica/src/main/java/ru/rustore/appmetrica/AppMetrica.kt
@@ -4,11 +4,14 @@ import com.yandex.metrica.AdRevenue
 import com.yandex.metrica.AdType
 import com.yandex.metrica.PreloadInfo
 import com.yandex.metrica.Revenue
+import com.yandex.metrica.Revenue.Receipt
 import com.yandex.metrica.YandexMetrica
 import com.yandex.metrica.YandexMetricaConfig
+import org.godotengine.godot.Dictionary
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
+import org.json.JSONObject
 import java.util.Currency
 
 
@@ -26,6 +29,10 @@ class AppMetrica(godot: Godot?) : GodotPlugin(godot) {
 
     @UsedByGodot
     fun init() {
+        if (godot == null) {
+            return
+        }
+
         YandexMetrica.activate(godot.requireContext(), config.build())
     }
 
@@ -62,7 +69,7 @@ class AppMetrica(godot: Godot?) : GodotPlugin(godot) {
     }
 
     @UsedByGodot
-    fun withPreloadInfo(name: String, params: org.godotengine.godot.Dictionary) {
+    fun withPreloadInfo(name: String, params: Dictionary) {
         val info = PreloadInfo.newBuilder(name)
         for (p in params) {
             if (p.value is String) {
@@ -110,17 +117,13 @@ class AppMetrica(godot: Godot?) : GodotPlugin(godot) {
     }
 
     @UsedByGodot
-    fun reportEvent(name: String, params: org.godotengine.godot.Dictionary) {
+    fun reportEvent(name: String, params: Dictionary) {
         YandexMetrica.reportEvent(name, params.toMutableMap())
     }
 
     @UsedByGodot
-    fun reportAdRevenue(revenue: Double, currency: String, params: org.godotengine.godot.Dictionary) {
+    fun reportAdRevenue(revenue: Double, currency: String, params: Dictionary) {
         val event = AdRevenue.newBuilder(revenue, Currency.getInstance(currency))
-
-        if (params.containsKey(AD_NETWORK)) {
-            event.withAdNetwork(params[AD_NETWORK].toString())
-        }
 
         if (params.containsKey(AD_TYPE)) {
             val ad = when (params.get(AD_TYPE) as String) {
@@ -134,33 +137,83 @@ class AppMetrica(godot: Godot?) : GodotPlugin(godot) {
             event.withAdType(ad)
         }
 
-        if (params.containsKey(AD_PRECISION)) {
-            event.withPrecision(params.get(AD_PRECISION) as String)
+        if (params.containsKey(AD_NETWORK)) {
+            event.withAdNetwork(params[AD_NETWORK].toString())
         }
 
-//        event.withAdPlacementId()
-//        event.withAdPlacementName()
-//        event.withAdUnitId()
-//        event.withAdUnitName()
-//        event.withPayload()
+        if (params.containsKey(AD_UNIT_ID)) {
+            event.withAdUnitId(params[AD_UNIT_ID].toString())
+        }
+
+        if (params.containsKey(AD_UNIT_NAME)) {
+            event.withAdUnitName(params[AD_UNIT_NAME].toString())
+        }
+
+        if (params.containsKey(AD_PLACEMENT_ID)) {
+            event.withAdPlacementId(params[AD_PLACEMENT_ID].toString())
+        }
+
+        if (params.containsKey(AD_PLACEMENT_NAME)) {
+            event.withAdPlacementName(params[AD_PLACEMENT_NAME].toString())
+        }
+
+        if (params.containsKey(AD_PRECISION)) {
+            event.withPrecision(params[AD_PRECISION].toString())
+        }
+
+        if (params.containsKey(PAYLOAD)) {
+            val json = JSONObject(params[PAYLOAD].toString())
+            val payload = json.toMap().map { e -> e.key to e.value.toString() }.toMap()
+
+            event.withPayload(payload)
+        }
 
         YandexMetrica.reportAdRevenue(event.build())
     }
 
     @UsedByGodot
-    fun reportRevenue() {
-        val revenue = Revenue.newBuilderWithMicros(99000000, Currency.getInstance("RUB"))
-            .withProductID("com.yandex.service.299")
-            .withQuantity(2) // Passing the OrderID parameter in the .withPayload(String payload) method to group purchases.
-            .withPayload("{\"OrderID\":\"Identifier\", \"source\":\"Google Play\"}")
-            .build()
+    fun reportRevenue(revenue: Long, currency: String, params: Dictionary) {
+        val revenue = Revenue.newBuilderWithMicros(revenue, Currency.getInstance(currency))
 
-        YandexMetrica.reportRevenue(revenue)
+        if (params.containsKey(REVENUE_QUANTITY)) {
+            revenue.withQuantity(params[REVENUE_QUANTITY] as? Int)
+        }
+
+        if (params.containsKey(REVENUE_PRODUCT_ID)) {
+            revenue.withProductID(params[REVENUE_PRODUCT_ID].toString())
+        }
+
+        if (params.containsKey(PAYLOAD)) {
+            revenue.withPayload(params[PAYLOAD].toString())
+        }
+
+        if (params.containsKey(REVENUE_RECEIPT)) {
+            val receipt: Receipt.Builder = Receipt.newBuilder()
+                .withData(params[REVENUE_RECEIPT].toString())
+            if  (params.containsKey(REVENUE_SIGNATURE)) {
+                receipt.withSignature(params[REVENUE_RECEIPT].toString())
+            }
+
+            revenue.withReceipt(receipt.build())
+        }
+
+        YandexMetrica.reportRevenue(revenue.build())
     }
 
     companion object {
         const val AD_NETWORK = "network"
         const val AD_TYPE = "type"
         const val AD_PRECISION = "precision"
+        const val AD_PLACEMENT_ID = "placement_id"
+        const val AD_PLACEMENT_NAME = "placement_name"
+        const val AD_UNIT_ID = "unit_id"
+        const val AD_UNIT_NAME = "unit_name"
+
+        const val REVENUE_QUANTITY = "quantity"
+        const val REVENUE_PRODUCT_ID = "product_id"
+        const val REVENUE_RECEIPT = "receipt"
+        const val REVENUE_SIGNATURE = "signature"
+
+        const val PAYLOAD = "payload"
     }
 }
